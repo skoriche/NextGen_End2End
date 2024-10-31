a275b210e6d95621cba1e9af996695774e78133c
Ahmad Jan <ajkhattak@gmail.com>
2024-10-31 17:51:47 -0400

    minor fixes.

--- src_r/custom_functions.R
+++ src_r/custom_functions.R
@@ -12,4 +12,4 @@ source(glue("{workflow_dir}/src_r/driver.R"))
 functions_lst = c("run_driver", "add_model_attributes", "dem_function", "twi_function", 
                   "width_function", "twi_pre_computed_function", "giuh_function", 
                   "Nash_Cascade_Runoff", "get_nash_params", "fun_crop_lower", 
-                  "fun_crop_upper")
+                  "fun_crop_upper", "clean_move_dem_dir")
